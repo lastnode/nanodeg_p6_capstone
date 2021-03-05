01db97e3ebfcb199a7bd17db078736750e5cde41
Mahangu Weerasinghe <mahangu@automattic.com>
2021-03-05 21:03:02 +0530

    Added sql for preliminary ECO codes.

--- lichess-spark.py
+++ lichess-spark.py
@@ -47,12 +47,19 @@ def load_json_files_to_spark(url):
                         createdAt,
                         id,
                         moves,
+                        case 
+                            when moves like "%e4 c5%" then "B20 Sicilian defence"
+                            when moves like "%e4 c5 f4%" then "B21 Sicilian, Grand Prix attack"
+                            when moves like "%e4 c5 Nf3%" then "B27 Sicilian defence"
+                            when moves like "%d4 d5 c4 dxc4%" then "D20 Queen's gambit accepted"
+                            when moves like "%d4 d5 c4 dxc4 Nf3%" then "D21 Queen's Gambit Accepted, 3.Nf3"                            
+                            when moves like "%d4 d5 c4 dxc4 Nf3 a6%" then "D22 Queen's Gambit Accepted, Alekhine defence"                            
+                            else null 
+                        end as opening,
                         players_black_user_name,
                         players_white_user_name,
                         winner
                     from lichess_main
-
-
 
         """)
 
