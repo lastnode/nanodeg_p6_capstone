ae460ceeb344e4e04aeac3de7c3122a303310529
Mahangu Weerasinghe <mahangu@automattic.com>
2021-03-24 18:13:27 +0530

    Moved df creation into try: block.

--- etl-staging.py
+++ etl-staging.py
@@ -32,41 +32,44 @@ def load_json_files_to_staging(url, nbgames):
 
     try:
         df = spark.createDataFrame(flattened_json_responses)
+        
+        df.show()
+
+        df.createOrReplaceTempView("lichess_api")
+
+        df.write.mode('append').parquet("output_data/" + "raw/lichess_api")
+
+        staging_table = spark.sql("""
+
+                        select
+                            from_unixtime(createdAt/1000, 'dd-MM-yyyy hh:mm:ss') as game_start,
+                            from_unixtime(lastMoveAt/1000, 'dd-MM-yyyy hh:mm:ss') as game_end,
+                            lastMoveAt/1000 - createdAt/1000 as game_length_seconds,
+                            id,
+                            speed,
+                            moves,
+                            opening_name,
+                            opening_ply,
+                            players_black_user_name as black_player_name,
+                            players_black_user_title as black_player_title,
+                            players_black_rating as black_player_rating,
+                            players_white_user_name as white_player_name,
+                            players_white_user_title as white_player_title,
+                            players_white_rating as white_player_rating,
+                            status,
+                            winner
+                        from lichess_api
+            """)
+
+        staging_table.show()
+
+        staging_table.write.mode('append').parquet("output_data/" + "staging/")
 
     except ValueError:
         pass
         
-    df.show()
 
-    df.createOrReplaceTempView("lichess_raw")
 
-    df.write.mode('append').parquet("output_data/" + "lichess_raw/")
-
-    staging_table = spark.sql("""
-
-                    select
-                        from_unixtime(createdAt/1000, 'dd-MM-yyyy hh:mm:ss') as game_start,
-                        from_unixtime(lastMoveAt/1000, 'dd-MM-yyyy hh:mm:ss') as game_end,
-                        lastMoveAt/1000 - createdAt/1000 as game_length_seconds,
-                        id,
-                        speed,
-                        moves,
-                        opening_name,
-                        opening_ply,
-                        players_black_user_name as black_player_name,
-                        players_black_user_title as black_player_title,
-                        players_black_rating as black_player_rating,
-                        players_white_user_name as white_player_name,
-                        players_white_user_title as white_player_title,
-                        players_white_rating as white_player_rating,
-                        status,
-                        winner
-                    from lichess_raw
-        """)
-
-    staging_table.show()
-
-    staging_table.write.mode('append').parquet("output_data/" + "staging/")
 
  
 
@@ -96,7 +99,7 @@ def main():
 
     players = ["alireza2003", "Konevlad", "neslraCsungaM77", "Vladimirovich9000", "sp1cycaterpillar", "Federicov93", "may6enexttime", "Kelevra317", "nihalsarin2004", " Drvitman", "DrNykterstein", "C9C9C9C9C9", "muisback", "Inventing_Invention", "RebeccaHarris", "drop_stone", "Alexander_Zubov", "IWANNABEADOORED", "Kelevra317", "dolar9", "cutemouse83"]
 
-    nbgames = 10
+    nbgames = 1000
 
     get_lichess_games(players, nbgames)
 
