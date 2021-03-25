7583b2f30d333d5ddb3e704ae4e0e11a22eeca4c
Mahangu Weerasinghe <mahangu@automattic.com>
2021-03-25 16:58:11 +0530

    Added nbgames var back in.

--- etl-staging.py
+++ etl-staging.py
@@ -15,7 +15,7 @@ def load_json_files_to_staging(url, nbgames):
     .appName("Ingesting Lichess API via Spark") \
     .getOrCreate()
 
-    params = {'opening': 'true'}
+    params = {'max': nbgames, 'opening': 'true'}
 
     headers = {'Accept': 'application/x-ndjson'}
 
@@ -75,7 +75,7 @@ def main():
 
     players = ["alireza2003", "Konevlad", "neslraCsungaM77", "Vladimirovich9000", "sp1cycaterpillar", "Federicov93", "may6enexttime", "Kelevra317", "nihalsarin2004", " Drvitman", "DrNykterstein", "C9C9C9C9C9", "muisback", "Inventing_Invention", "RebeccaHarris", "drop_stone", "Alexander_Zubov", "IWANNABEADOORED", "Kelevra317", "dolar9", "cutemouse83"]
 
-    nbgames = 1000
+    nbgames = 10
 
     get_lichess_games(players, nbgames)
 
