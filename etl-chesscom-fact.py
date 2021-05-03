0ddb400eb5915939f5b284f07239188476a26b1e
Mahangu Weerasinghe <mahangu@automattic.com>
2021-05-03 19:28:16 +0530

    Refactored etl-chesscom-fact.py to contain a main() function.

--- etl-chesscom-fact.py
+++ etl-chesscom-fact.py
@@ -1,13 +1,17 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
 from pyspark import SQLContext
 import argparse
 import yaml
+import os
 from pgn_parser import parser, pgn
 
 with open(r'dl-chesscom.yaml') as file:
-    config = yaml.load(file)
+    config = yaml.load(file,Loader=yaml.SafeLoader)
 
+os.environ['AWS_ACCESS_KEY_ID']=config['aws_access_key_id']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['aws_secret_key_id']
 
 def get_eco_from_pgn(pgn_text):
 
@@ -48,82 +52,75 @@ def get_moves_from_pgn(pgn_text):
             pass
 
 
-spark = SparkSession \
-    .builder \
-    .appName("Ingesting Lichess API via Spark") \
-    .getOrCreate()
+def main():
 
-spark.udf.register("get_eco_from_pgn", get_eco_from_pgn)
-spark.udf.register("get_termination_from_pgn", get_termination_from_pgn)
-spark.udf.register("get_moves_from_pgn", get_moves_from_pgn)
-
-parser = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
     prog='etl-staging.py',
     description="""ETL Script that extracts data from
         Lichess API and loads them into a staging table in
         parquet files.""")
 
-parser.add_argument(
-    '-l', '--local',
-    action='store_true',
-    help="""Save data locally instead of outputting to s3.""")
+    parser.add_argument(
+        '-l', '--local',
+        action='store_true',
+        help="""Save data locally instead of outputting to s3.""")
 
-args, _ = parser.parse_known_args()
+    args, _ = parser.parse_known_args()   
 
-if args.local:
-    output_data = config['output_data_path_local']
-else:
-    output_data = config['output_data_path_s3']
+    if args.local:
+        output_data = config['output_data_path_local']
+    else:
+        output_data = config['output_data_path_s3']
 
-df = spark.read.parquet(output_data + "staging/chessdotcom_local_v3/1*")
+    spark = SparkSession \
+        .builder \
+        .appName("Ingesting Chess.com API via Spark") \
+        .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.0')\
+        .getOrCreate()
 
-df.createOrReplaceTempView("chessdotcom_staging")
+    spark.conf.set("mapreduce.fileoutputcommitter.algorithm.version", "2") 
 
-moves_table = spark.sql("""
+    spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider","org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
+    spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.access.key",config['aws_access_key_id'])
+    spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key",config['aws_secret_key_id'])
 
-                        select
-                            from_unixtime(end_time, 'yyyy-MM-dd hh:mm:ss') as game_end_time,
-                            from_unixtime(end_time, 'yyyy-MM-dd') as game_end_date,
-                            time_class,
-                            rated,
-                            white_username,
-                            white_rating,
-                            black_username,
-                            black_rating,
-                            case
-                                when white_result = "win" then "white"
-                                when black_result = "win" then "black"
-                                when (white_result = "agree" or black_result = "agree") then "draw"
-                                when (white_result = "stalemate" or black_result = "stalemate") then "stalemate"
-                            end as winner,
-                            get_termination_from_pgn(pgn) as termination,
-                            get_eco_from_pgn(pgn) as opening,
-                            get_moves_from_pgn(pgn) as moves,
-                            'chessdotcom' as platform
-                        from chessdotcom_staging
 
-                        order by game_end_time
+    spark.udf.register("get_eco_from_pgn", get_eco_from_pgn)
+    spark.udf.register("get_termination_from_pgn", get_termination_from_pgn)
+    spark.udf.register("get_moves_from_pgn", get_moves_from_pgn)
 
-            """)
+    df = spark.read.parquet(output_data + "raw/chessdotcom/*")
 
-#moves_table_cleaned = moves_table.dropDuplicates(['id'])
+    df.createOrReplaceTempView("chessdotcom_staging")
 
-#moves_table_cleaned.show()
+    games_table = spark.sql("""
 
-moves_table.write.mode('append').parquet(output_data +"fact/chessdotcom/" + "moves6/")
+                            select
+                                from_unixtime(end_time, 'yyyy-MM-dd hh:mm:ss') as game_end_time,
+                                from_unixtime(end_time, 'yyyy-MM-dd') as game_end_date,
+                                time_class,
+                                rated,
+                                white_username,
+                                white_rating,
+                                black_username,
+                                black_rating,
+                                case
+                                    when white_result = "win" then "white"
+                                    when black_result = "win" then "black"
+                                    when (white_result = "agree" or black_result = "agree") then "draw"
+                                    when (white_result = "stalemate" or black_result = "stalemate") then "stalemate"
+                                end as winner,
+                                get_termination_from_pgn(pgn) as termination,
+                                get_eco_from_pgn(pgn) as opening,
+                                get_moves_from_pgn(pgn) as moves,
+                                'chessdotcom' as platform
+                            from chessdotcom_staging
 
-# 
-# openings_table = spark.sql("""
-# 
-# select
-# get_eco_from_pgn(pgn) as eco,
-# count(*) as game_count
-# from chessdotcom_staging
-# 
-# group by eco
-# order by game_count
-# 
-# """)
-# 
-# openings_table.write.mode('append').parquet(output_data +"fact/" + "openings5/")
+                            order by game_end_time
 
+                """)
+
+    games_table.write.mode('append').parquet(output_data +"staging/chessdotcom/" + "games/")
+
+if __name__ == "__main__":
+    main()
