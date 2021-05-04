3b2fed09cdf87fe203fca9083faf1c40fe2f3c6d
Mahangu Weerasinghe <mahangu@automattic.com>
2021-05-04 21:52:42 +0530

    Added docstrings and s3 write support.

--- etl-api-chessdotcom.py
+++ etl-api-chessdotcom.py
@@ -2,16 +2,24 @@ import pandas as pd
 from datetime import datetime
 import yaml, argparse, os, requests, json
 from chessdotcom import get_player_games_by_month, get_player_game_archives
+import s3fs
 
 
-with open(r'dl-chesscom.yaml') as file:
-    config = yaml.load(file)
+def load_json_files_to_staging(url, local, config):
 
-os.environ['AWS_ACCESS_KEY_ID']=config['aws_access_key_id']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['aws_secret_key_id']
+    """
+    Fetches data from the Chess.com URL passed to it
+    and savesit as a parquet file.
+    
+    Params: 
+    url = the Chedss.com API URL that is fetched
+    local = Whether the file should be saved locally (True or False)
+    config = the config object (the contents of the yaml file)
+    
+    Returns:
+    None
 
-
-def load_json_files_to_staging(url, local):
+    """
 
     print("Fetching data from: " + url)
 
@@ -47,14 +55,30 @@ def load_json_files_to_staging(url, local):
 
     url_split = url.split("/")
 
-    pd_df.to_parquet(output_data + "staging/chessdotcom_local_v3/" + url_split[5] + "_" + url_split[7] + "_" + url_split[8] + '.parquet')
+    if local == True:
+        
+        pd_df.to_parquet(output_data + "raw/chessdotcom/" + url_split[5] + "_" + url_split[7] + "_" + url_split[8] + '.parquet')
 
-    #spark_main_df = spark.createDataFrame(pd_df)
+    elif local == False:
+        
+        pd_df.to_parquet("s3://" + output_data + "raw/chessdotcom/" + url_split[5] + "_" + url_split[7] + "_" + url_split[8] + '.parquet')
 
-    #spark_main_df.write.mode('append').parquet(output_data + "staging/chessdotcom_local_v3/")
 
+def get_chesscom_games(player_list, local, config):
 
-def get_chesscom_games(player_list, max_games, local):
+    """
+    Gets the monthly archive URLs of all games played by
+    a player and then recursively calls load_json_files_to_staging()
+    on all those monthly archive URLs.
+    
+    Params: 
+    player_list = list of player usernames (passed in from config file)
+    local = Whether the file should be saved locally (True or False)
+    config = the config object (the contents of the yaml file)
+    
+    Returns:
+    None
+    """
 
     for player in player_list:
 
@@ -64,16 +88,37 @@ def get_chesscom_games(player_list, max_games, local):
 
             for url in urls:
 
-                load_json_files_to_staging(url, local)
+                load_json_files_to_staging(url, local, config)
 
 
 def main():
 
+    """
+    The main function of the ETL API script. 
+
+    Gets the list of players from the yaml file and
+    reads command line  arguments via argparse, to see
+    whether the user wants save the data locally or on s3.
+
+    Thereafter, calls get_chesscom_games().
+    
+    Params: 
+    None
+    
+    Returns:
+    None
+    """
+
+    with open(r'dl-chesscom.yaml') as file:
+        config = yaml.load(file)
+
+    os.environ['AWS_ACCESS_KEY_ID']=config['aws_access_key_id']
+    os.environ['AWS_SECRET_ACCESS_KEY']=config['aws_secret_key_id']
+
     parser = argparse.ArgumentParser(
         prog='etl-staging.py',
         description="""ETL Script that extracts data from
-            Lichess API and loads them into a staging table in
-            parquet files.""")
+            Chess.com API and saves them in parquet files.""")
 
     parser.add_argument(
         '-l', '--local',
@@ -91,9 +136,9 @@ def main():
     if args.local:
         print("Saving outputd data locally instead of writing to s3 bucket.")
 
-        get_chesscom_games(players, max_games, True)
+        get_chesscom_games(players, True, config)
     else:
-        get_chesscom_games(players, max_games, False)
+        get_chesscom_games(players, False, config)
 
 if __name__ == "__main__":
     main()
